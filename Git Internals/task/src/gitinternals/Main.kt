c63f19f6c39820f7ab2d3d1a90e632f2137731d3
denys <denamyte@gmail.com>
2023-11-17 00:22:12 +0200

    2023-11-17: Stage 1/7

--- Git Internals/task/src/gitinternals/Main.kt
+++ Git Internals/task/src/gitinternals/Main.kt
@@ -1,5 +1,22 @@
 package gitinternals
 
+import java.io.FileInputStream
+import java.util.zip.InflaterInputStream
+
+private val Int.b get() = this.toByte()
+private val BYTE_CHANGE = mapOf(0x0.b to 0xA.b)  // NULL to \n
+
 fun main() {
-    // write your code here
+    println("Enter git object location:")
+    val path = readln()
+
+    FileInputStream(path).use {
+        InflaterInputStream(it).use { iis ->
+            iis.readAllBytes()
+                .map { b -> BYTE_CHANGE.getOrDefault(b, b) }
+                .toByteArray()
+                .let { bAr -> String(bAr) }
+                .let(::print)
+        }
+    }
 }
