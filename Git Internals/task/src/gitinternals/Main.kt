3f0606c2e81cb8d02ddf3bfc3663377fddbc365f
denys <denamyte@gmail.com>
2023-11-17 01:35:52 +0200

    2023-11-17: Stage 2/7

--- Git Internals/task/src/gitinternals/Main.kt
+++ Git Internals/task/src/gitinternals/Main.kt
@@ -4,19 +4,30 @@ import java.io.FileInputStream
 import java.util.zip.InflaterInputStream
 
 private val Int.b get() = this.toByte()
-private val BYTE_CHANGE = mapOf(0x0.b to 0xA.b)  // NULL to \n
 
 fun main() {
-    println("Enter git object location:")
-    val path = readln()
+    println("Enter .git directory location:")
+    val dir = readln()
+    println("Enter git object hash:")
+    val hash = readln()
 
-    FileInputStream(path).use {
-        InflaterInputStream(it).use { iis ->
-            iis.readAllBytes()
-                .map { b -> BYTE_CHANGE.getOrDefault(b, b) }
-                .toByteArray()
-                .let { bAr -> String(bAr) }
-                .let(::print)
-        }
+    val path = makePath(dir, hash)
+    val header = readHeader(path)
+    println(formatHeader(header))
+}
+
+fun makePath(dir: String, hash: String) =
+    with(hash) { "$dir/objects/${take(2)}/${substring(2)}" }
+
+
+fun readHeader(path: String) =
+    InflaterInputStream(FileInputStream(path)).use { iis ->
+        iis.readAllBytes().takeWhile { it != 0.b }
+            .toByteArray()
+            .let { bAr -> String(bAr) }
     }
+
+fun formatHeader(header: String): String {
+    val split = header.split(" ")
+    return "type:${split[0]} length:${split[1]}"
 }
